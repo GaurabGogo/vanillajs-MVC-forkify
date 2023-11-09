8a18ec9df173c57c58d8ec89d5ddf74a47fe43dd
gaurabgogo <gaurabgogo@gmail.com>
2023-11-09 14:55:56 +0545

    initaial commit

---
+++ src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = '42fa9469-4631-4238-8077-8ed798cf21b4';
+export const MODAL_CLOSE_SEC = 2.5;
