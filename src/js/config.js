4414e745067a71806a68169f3fc923ddf0271887
Gaurab Chaudhary <97524121+GaurabGogo@users.noreply.github.com>
2024-01-28 03:36:18 +0545

    Update config.js

--- src/js/config.js
+++ src/js/config.js
@@ -1,4 +1,4 @@
-export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
 export const KEY = '42fa9469-4631-4238-8077-8ed798cf21b4';
